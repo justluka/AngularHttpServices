89ee1fe16a77571d852a2a6d61ea62fdcae6365a
Luis Carranza <lukanew@Luiss-MacBook-Pro.local>
2017-11-21 23:32:17 -0500

    Module 107

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -7,13 +7,28 @@ import { Http } from '@angular/http';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent  {
-  post: any[];
-  constructor(http: Http) {
-      http.get('http://jsonplaceholder.typicode.com/posts')
+  posts: any[];
+  private url='http://jsonplaceholder.typicode.com/posts';
+
+  constructor(private http: Http) {
+      http.get(this.url)
       .subscribe(response => {
-          this.post=response.json();
+          this.posts=response.json();
       });
    }
 
+   createPost(input: HTMLInputElement){
+      let post = { title: input.value};
+      input.value='';
+
+      this.http.post(this.url,JSON.stringify(post))
+      .subscribe(response  =>{
+          post['id']=response.json().id;
+          this.posts.splice(0,0,post);
+
+          console.log(response.json());
+      });
+
+   }
 
 }
